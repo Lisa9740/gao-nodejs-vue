12b57dea7f5715ec07ac74fe2746d79b0b4ed7c6
alison <barretalison@gmail.com>
2020-12-02 02:41:07 -0800

    Added User Seeder for project

---
+++ server/seeders/20201201160649-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Users', [{
+      email : 'admin@gmail.com' ,
+      password : await bcrypt.hash('test', 12),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+    ], {});
+  },
+
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
