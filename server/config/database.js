7a24c45d8ed2fbccb95bf3f874e98228e1518308
root <root@vps-775634f9.vps.ovh.net>
2021-11-15 09:55:26 +0000

    Fixed env variable mistype

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@
 const dotenv = require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
+const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
